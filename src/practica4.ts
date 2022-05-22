fbcabe8266c8f5e8316d0eb674c63b42e6c7ae2e
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-05-22 20:12:06 -0300

    Descuento Descuento de la compra

---
+++ src/practica4.ts
@@ -0,0 +1,26 @@
+/*•Desarrolle un algoritmo que diga el precio de
+una compra
+• La compra se compone del precio del producto
+y la cantidad
+•Si el cliente gasta más de $1000 debemos
+aplicarle un descuento del 10%*/
+
+let precioDelProducto: number = Number(
+  prompt("Ingrese el precio del producto")
+);
+let cantidadDeCompra: number = Number(
+  prompt("Ingrese la cantidad de producto que compro")
+);
+let descuento: number = 0;
+let precioTotal: number = 0;
+let precioFinal: number = 0;
+
+precioTotal = precioDelProducto * cantidadDeCompra;
+
+if (precioTotal > 1000) {
+  descuento = precioTotal * 0.1;
+  precioFinal = precioTotal - descuento;
+  console.log("El precio con descuento es el siguiente: ", precioFinal);
+} else {
+  console.log("Su compra NO tiene descuento");
+}
