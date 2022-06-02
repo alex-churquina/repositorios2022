942b26f78eb43b29a207a3652484bf3be157de64
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-06-02 00:20:15 -0300

    Distribución de positivos porcentaje

---
+++ src/practica1.7.ts
@@ -0,0 +1,5 @@
+/*Leer valores del usuario hasta que introduzca un 0
+• El usuario puede introducir valores numéricos, tanto
+positivos como negativos
+• Contar la cantidad de valores introducidos que sean
+mayores a 0 y el porcentaje de positivos respecto del total*/
