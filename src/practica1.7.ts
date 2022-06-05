6dc7b44b4c4e94d74743e9601a2146b6a2389e96
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-06-05 17:48:30 -0300

    Descuento de compra compras con descuento

--- src/practica1.7.ts
+++ src/practica1.7.ts
@@ -7,6 +7,7 @@ mayores a 0 y el porcentaje de positivos respecto del total*/
 let numero: number;
 let continuar: number = 1;
 let numerosPositivos: number = 0;
+let numerosNegativos: number = 0;
 let totalNumeros: number = 0;
 let promedio: number = 0;
 
@@ -14,10 +15,13 @@ while (continuar !== 0) {
   numero = Number(prompt("Ingrese un numero"));
   if (numero > 0) {
     numerosPositivos++;
+  } else if (numero < 0) {
+    numerosNegativos++;
   }
   totalNumeros++;
   continuar = Number(prompt("Desea continuar? presione 1"));
 }
 promedio = (numerosPositivos * 100) / totalNumeros;
-console.log("Cantidad de numeros positivos: " + numerosPositivos);
+console.log("Cantidad de numeros positivos: ", numerosPositivos);
+console.log("Cantidad de numeros negativos ingresados: ", numerosNegativos);
 console.log("El porcentaje de numeros positivos son: ", promedio, "%");
---
+++ src/practica1.8.ts
@@ -0,0 +1,8 @@
+/*•Desarrolle un algoritmo que diga el precio de
+una compra
+• La compra se compone del precio del producto
+y la cantidad
+•Si el cliente gasta más de $1000 debemos
+aplicarle un descuento del 10%*/
+
+let comprasDelDia: number = Number(prompt("Ingrese el precio"));
