6ff9ae1c0bd9c77ac69c5aeaff0cae89f1e7a000
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-06-13 21:14:10 -0300

    Compra Descuento

--- src/practica1.8.ts
+++ src/practica1.8.ts
@@ -5,4 +5,23 @@ y la cantidad
 •Si el cliente gasta más de $1000 debemos
 aplicarle un descuento del 10%*/
 
-let comprasDelDia: number = Number(prompt("Ingrese el precio"));
+let continuar: number = 1;
+let comprasDelDia: number;
+let porcentajeDescuento: number = 0.1;
+let descuento: number = 0;
+let compraFinal: number = 0;
+
+while(continuar !== 0){
+  comprasDelDia = Number(prompt("Ingrese la compra realizada"));
+  if(comprasDelDia > 1000){
+    console.log("Su compra le correspode un 10% de descuento");
+    descuento = porcentajeDescuento * comprasDelDia;
+    compraFinal = comprasDelDia - descuento;
+    console.log("Su compra final con descuento es: " + compraFinal)
+  }else if(comprasDelDia < 1000){
+    console.log("Su compra es inferior a 1000");
+    console.log("Para obtener dicho descuento debe superar los 1000");
+  }
+  continuar = Number(prompt("Desea seguir ingresando compras"));
+}
+console.log("Gracias por visitarnos, Vuelva pronto");
