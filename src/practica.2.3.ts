c353d72ea154216f7f734082d5115a702a53aefb
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-06-16 00:46:26 -0300

    Eureka contraseña eureka

---
+++ src/practica.2.3.ts
@@ -0,0 +1,13 @@
+/*Escribir un algoritmo que nos pida
+una clave y verifique que sea la
+correcta
+• Tenga en cuenta que la clave es la
+palabra “eureka”
+• Solo tenemos 3 intentos para
+acertar, si fallamos los 3 intentos
+el sistema mostrará un mensaje
+indicándonos que hemos agotado
+todas las oportunidades
+• Si acertamos la clave, saldremos
+directamente del programa*/
+let clave: string;
