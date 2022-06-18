ea8ca9c737d6a846c9a27db3e7a60f7aa4c69103
alex-churquina <104175773+alex-churquina@users.noreply.github.com>
2022-06-17 23:12:58 -0300

    Porcentaje de Positivos Porcentual de numeros positivos

---
+++ src/practica.2.7.ts
@@ -0,0 +1,19 @@
+/*Leer valores del usuario hasta que introduzca un 0
+• El usuario puede introducir valores numéricos, tanto
+positivos como negativos.
+• Contar la cantidad de valores introducidos que sean
+mayores a 0 y el porcentaje de positivos respecto del total*/
+
+let numero: number;
+let numerosPositivos: number = 0;
+let numerosNegativos: number = 0;
+let cantidadDeNumeros: number = 0;
+let contador: number = 0;
+let continuar: number = 1;
+
+while (contador !== 0) {
+  numero = Number(prompt("Ingrese cualquier numero + o -"));
+  continuar = Number(prompt("¿Desea continuar?"));
+  if (numero < 0) {
+  }
+}
